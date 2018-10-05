dab6c664192c674d3ee63cf34ec90c85a248bf8d
Jon <jonathan.mcneil.90@gmail.com>
2018-10-05 16:05:28 -0400

    fixed FaceRecognition component error

--- src/App.js
+++ src/App.js
@@ -38,19 +38,19 @@ class App extends Component {
   }
 
   onButtonSubmit = () => {
-    this.setState({imageUrl: this.state.input});
-    app.models.predict(
-      Clarifai.FACE_DETECT_MODEL, 
-      this.state.input).then(
-    function(response) {
-      console.log(response);
-    },
-    function(err) {
-      // there was an error
-    }
-  );
-
+      this.setState({imageUrl: this.state.input});
+      app.models.predict(
+        Clarifai.FACE_DETECT_MODEL, 
+        this.state.input).then(
+      function(response) {
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+      },
+      function(err) {
+        // there was an error
+      }
+    );
   }
+
   render() {
     return (
       <div className="App">
